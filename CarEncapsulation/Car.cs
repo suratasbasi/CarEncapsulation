64f51fad2003e6d7d0a75e4e7452584492ba188e
suratasbasii@gmail.com <suratasbasii@gmail.com>
2024-08-12 12:24:37 +0300

    Add project files.

---
+++ CarEncapsulation/Car.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encapsulation
+{
+    public class Car
+    {
+        public string _brand;
+        public string _model;
+        public string _colour;
+        private int _numberOfDoors;
+
+
+        public Car(string brand, string model, string colour, int numberOfDoors)
+        {
+            _brand = brand;
+            _model = model;
+            _colour = colour;
+            NumberOfDoors = numberOfDoors;
+        }
+
+        public string Brand { get { return _brand; } }
+
+        public string Model { get { return _model; } }
+
+        public string Colour { get { return _colour; } }
+
+        public int NumberOfDoors
+        {
+            get { return _numberOfDoors; }
+
+            set
+            {
+                if (value == 2 || value == 4)
+                    _numberOfDoors = value;
+                else
+                {
+                    _numberOfDoors = -1;
+                }
+            }
+        }
+
+        public void CarInformation()
+        {
+            Console.WriteLine("Aracın markası: " + _brand + "\nModeli: " + _model + "\nRengi: " + _colour + "\nKapı sayısı: " + _numberOfDoors);
+
+            if (_numberOfDoors == -1)
+                Console.WriteLine("Kapı sayısı 2 veya 4 olmalıdır.");
+        }
+
+    }
+}
---
+++ CarEncapsulation/Program.cs
@@ -0,0 +1,13 @@
+﻿using Encapsulation;
+
+public class Program
+{
+    public static void Main()
+    {
+        Car car1 = new Car("Hyundai", "I20", "Siyah", 4);
+        car1.CarInformation();
+
+        Car car2 = new Car("Citroen", "C3", "Beyaz", 3);
+        car2.CarInformation();
+    }
+}
